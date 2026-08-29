f6df6f20c40808bae6ceb51798478350ea978095
Geot125 <thanasarasgeorge@gmail.com>
2026-08-29 14:40:18 +0300

    Add read_manifest function to parse manifest.json

--- gtpm.py
+++ gtpm.py
@@ -1,6 +1,7 @@
 import tarfile
 import shutil
 import os
+import json
 
 def extract_package(tarball_path, dest=None):
     if dest is None:
@@ -11,6 +12,14 @@ def extract_package(tarball_path, dest=None):
         tar.extractall(dest)
     return dest
 
+def read_manifest(extracted_path):
+    manifest_path = os.path.join(extracted_path, "manifest.json")
+    with open(manifest_path, "r") as f:
+        data = json.load(f)
+    return data
+
 if __name__ == "__main__":
     extracted_path = extract_package("mytool-1.0.0.tar.gz")
     print(f"Extraction done, check {extracted_path}")
+    manifest = read_manifest(extracted_path)
+    print(manifest)
