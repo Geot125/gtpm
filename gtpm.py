787d531380d10ba36cf254a76d46257ac5a1e283
Geot125 <thanasarasgeorge@gmail.com>
2026-08-29 13:51:40 +0300

    Add extract_package function

---
+++ gtpm.py
@@ -0,0 +1,13 @@
+import tarfile
+import shutil
+import os
+
+def extract_package(tarball_path, dest="/tmp/gtpm-extract"):
+    if os.path.exists(dest):
+        shutil.rmtree(dest)
+    with tarfile.open(tarball_path, "r:gz") as tar:
+        tar.extractall(dest)
+
+if __name__ == "__main__":
+    extract_package("mytool-1.0.0.tar.gz")
+    print("Extraction done, check /tmp/gtpm-extract")
